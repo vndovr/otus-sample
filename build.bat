0c28dc5ee928ddff73eb3363712f53602d87249a
Uladzimir Radchuk <radchuk@ibagroup.eu>
2021-02-23 20:14:34 +0100

    Version 3 of container

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-docker build . -t vndovr/otus-sample:latest --no-cache
+call mvn clean install
+docker build . -t vndovr/otus-sample:v3 --no-cache
+docker push vndovr/otus-sample:v3
--- run.bat
+++
@@ -1,1 +0,0 @@
-docker run -it -p 8080:8000 vndovr/otus-sample
